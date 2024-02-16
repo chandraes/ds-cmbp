09398bfa08dd5253026c3140c037965356333a4f
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-02-17 00:03:15 +0700

    Add GroupWaNew seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             RekeningSeeder::class,
             GroupWaSeeder::class,
             RekeningTambahSeeder::class,
+            GroupWaNew::class,
         ]);
     }
 }
