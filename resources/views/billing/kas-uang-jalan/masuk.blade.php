4434ec62b9a06fb5ffe47ac10af396b34ad4882e
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-03-12 17:07:49 +0700

    Remove unused jQuery library and update script source

--- resources/views/billing/kas-uang-jalan/masuk.blade.php
+++ resources/views/billing/kas-uang-jalan/masuk.blade.php
@@ -94,8 +94,6 @@
 </div>
 @endsection
 @push('js')
-{{-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script> --}}
-    <script src="{{asset('assets/js/jquery.maskMoney.js')}}"></script>
     <script>
         $(function() {
             var nominal = new Cleave('#nominal_transaksi', {
