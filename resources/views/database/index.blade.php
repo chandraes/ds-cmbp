e79f956dd90f1aa7034ab47821005e4094dc8dd5
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-09-04 15:10:28 +0700

    chore: Update SVG icons and home view for new features

--- resources/views/database/index.blade.php
+++ resources/views/database/index.blade.php
@@ -3,104 +3,134 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12 text-center">
-         <h1><u>Database</u></h1>
+         <h1><u>DATABASE</u></h1>
         </div>
     </div>
     <div class="row justify-content-left">
-        <h3>Customer</h3>
-        @if (auth()->user()->role === 'admin')
-        <div class="col-md-3 text-center mt-3 mb-3">
-            <a href="{{route('rute.index')}}" class="text-decoration-none">
-                <img src="{{asset('images/rute.svg')}}" alt="" width="80">
-                <h3>Rute</h3>
-            </a>
-        </div>
-        <div class="col-md-3 text-center mt-3 mb-3">
-            <a href="{{route('customer.index')}}" class="text-decoration-none">
-                <img src="{{asset('images/company.svg')}}" alt="" width="80">
-                <h3>Customer</h3>
-            </a>
-        </div>
-        {{-- <div class="col-md-3 text-center mt-3 mb-3">
-            <a href="{{route('bbm-storing.index')}}" class="text-decoration-none">
-                <img src="{{asset('images/bbm.svg')}}" alt="" width="80">
-                <h3>BBM Storing</h3>
-            </a>
-        </div> --}}
-        @endif
-    </div>
-    <hr>
-    <div class="row justify-content-left">
-        <h3>VENDOR</h3>
-        <div class="col-md-3 text-center mt-3 mb-3">
-            <a href="{{route('sponsor.index')}}" class="text-decoration-none">
-                <img src="{{asset('images/sponsor.svg')}}" alt="" width="80">
-                <h3>Sponsor</h3>
-            </a>
-        </div>
-        <div class="col-md-3 text-center mt-3 mb-3">
-            <a href="{{route('vendor.index')}}" class="text-decoration-none">
-                <img src="{{asset('images/vendor.svg')}}" alt="" width="80">
-                <h3>Vendor</h3>
-            </a>
-        </div>
-        <div class="col-md-3 text-center mt-3 mb-3">
-            <a href="{{route('vehicle.index')}}" class="text-decoration-none">
-                <img src="{{asset('images/dumptruckempty.svg')}}" alt="" width="80">
-                <h3>Vehicle</h3>
-            </a>
-        </div>
-        <div class="col-md-3 text-center mt-3 mb-3">
-            <a href="{{route('database.upah-gendong')}}" class="text-decoration-none">
-                <img src="{{asset('images/upah-gendong.svg')}}" alt="" width="80">
-                <h3 class="mt-3">UPAH GENDONG</h3>
-            </a>
-        </div>
-    </div>
-    <hr>
-    <div class="row justify-content-left">
-        <h3>BIODATA</h3>
-        <div class="col-md-3 text-center mt-3 mb-3">
-            <a href="{{route('karyawan.index')}}" class="text-decoration-none">
-                <img src="{{asset('images/karyawan.svg')}}" alt="" width="80">
-                <h3>Staff</h3>
-            </a>
-        </div>
-        <div class="col-md-3 text-center mt-3 mb-3">
-            <a href="{{route('direksi.index')}}" class="text-decoration-none">
-                <img src="{{asset('images/direksi.svg')}}" alt="" width="80">
-                <h3>Direksi</h3>
-            </a>
-        </div>
-        <div class="col-md-3 text-center mt-3 mb-3">
+        <h2>Data Lama</h2>
+        <div class="col-md-2 text-center mt-3 mb-3">
             <a href="{{route('pemegang-saham.index')}}" class="text-decoration-none">
-                <img src="{{asset('images/saham.svg')}}" alt="" width="80">
-                <h3>Pemegang Saham</h3>
+                <img src="{{asset('images/saham.svg')}}" alt="" width="70">
+                <h4 class="mt-3">PEMEGANG SAHAM</h4>
             </a>
         </div>
     </div>
     <hr>
     <div class="row justify-content-left">
-        <h3>OTHERS</h3>
-        <div class="col-md-3 text-center mt-3 mb-3">
-            <a href="{{route('rekening.index')}}" class="text-decoration-none">
-                <img src="{{asset('images/akun-bank.svg')}}" alt="" width="80">
-                <h3>Nomor Rekening Transaksi</h3>
+        <h2>Data Internal</h2>
+        <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
+            <a href="#" class="text-decoration-none">
+                <img src="{{asset('images/kosong.svg')}}" alt="" width="70">
+                <h4 class="mt-3">PERSENTASE DIVIDEN<br>PENGELOLA & INVESTOR</h4>
             </a>
         </div>
-        {{-- <div class="col-md-3 text-center mt-3 mb-3">
-            <a href="{{route('kategori-barang.index')}}" class="text-decoration-none">
-                <img src="{{asset('images/stock.svg')}}" alt="" width="80">
-                <h3>Kategori Barang</h3>
+        <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
+            <a href="#" class="text-decoration-none">
+                <img src="{{asset('images/kosong.svg')}}" alt="" width="70">
+                <h4 class="mt-3">PERSENTASE DIVIDEN<br>PENGELOLA</h4>
             </a>
-        </div> --}}
-
-        <div class="col-md-3 text-center mt-3 mb-3">
-            <a href="{{route('home')}}" class="text-decoration-none">
-                <img src="{{asset('images/dashboard.svg')}}" alt="" width="80">
-                <h3>Dashboard</h3>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
+            <a href="#" class="text-decoration-none">
+                <img src="{{asset('images/kosong.svg')}}" alt="" width="70">
+                <h4 class="mt-3">PERSENTASE DIVIDEN<br>INVESTOR</h4>
+            </a>
+        </div>
+        <div class="col-md-2 text-center mt-3 mb-3">
+            <a href="{{route('direksi.index')}}" class="text-decoration-none">
+                <img src="{{asset('images/direksi.svg')}}" alt="" width="70">
+                <h4 class="mt-3">BIODATA & GAJI<br>DIREKSI</h4>
+            </a>
+        </div>
+        <div class="col-md-2 text-center mt-3 mb-3">
+            <a href="{{route('karyawan.index')}}" class="text-decoration-none">
+                <img src="{{asset('images/karyawan.svg')}}" alt="" width="70">
+                <h4 class="mt-3">BIODATA & GAJI<br>STAFF</h4>
+            </a>
+        </div>
+        <div class="col-md-2 text-center mt-3 mb-3">
+            <a href="{{route('sponsor.index')}}" class="text-decoration-none">
+                <img src="{{asset('images/sponsor.svg')}}" alt="" width="70">
+                <h4 class="mt-3">BONUS STAFF</h4>
             </a>
         </div>
     </div>
+    <hr>
+    <div class="row justify-content-left">
+        <h2>Data Eksternal</h2>
+        <div class="col-md-2 text-center mt-3 mb-3">
+            <a href="{{route('vendor.index')}}" class="text-decoration-none">
+                <img src="{{asset('images/vendor.svg')}}" alt="" width="70">
+                <h4 class="mt-3">BIODATA<br>VENDOR</h4>
+            </a>
+        </div>
+        <div class="col-md-2 text-center mt-3 mb-3">
+            <a href="{{route('customer.index')}}" class="text-decoration-none">
+                <img src="{{asset('images/company.svg')}}" alt="" width="70">
+                <h4 class="mt-3">BIODATA CUSTOMER</h4>
+            </a>
+        </div>
+        <div class="col-md-2 text-center mt-3 mb-3">
+            <a href="{{route('rekening.index')}}" class="text-decoration-none">
+                <img src="{{asset('images/akun-bank.svg')}}" alt="" width="70">
+                <h4 class="mt-3">REKENING TRANSAKSI</h4>
+            </a>
+        </div>
+        <div class="col-md-2 text-center mt-3 mb-3">
+            <a href="#" class="text-decoration-none">
+                <img src="{{asset('images/kosong.svg')}}" alt="" width="70">
+                <h4 class="mt-3">PERSENTASE PAJAK</h4>
+            </a>
+        </div>
+    </div>
+    <hr>
+    <div class="row justify-content-left">
+        <h2>Data Kategori</h2>
+        <div class="col-md-2 text-center mt-3 mb-3">
+            <a href="#" class="text-decoration-none">
+                <img src="{{asset('images/kosong.svg')}}" alt="" width="70">
+                <h4 class="mt-3">KATEGORI COST OPERATIONAL</h4>
+            </a>
+        </div>
+        <div class="col-md-2 text-center mt-3 mb-3">
+            <a href="#" class="text-decoration-none">
+                <img src="{{asset('images/kosong.svg')}}" alt="" width="70">
+                <h4 class="mt-3">KATEGORI INVENTARIS</h4>
+            </a>
+        </div>
+    </div>
+    <hr>
+    <div class="row justify-content-left">
+        <h2>Data Transaksi</h2>
+        @if (auth()->user()->role === 'admin' || auth()->user()->role === 'su')
+        <div class="col-md-2 text-center mt-3 mb-3">
+            <a href="{{route('rute.index')}}" class="text-decoration-none">
+                <img src="{{asset('images/rute.svg')}}" alt="" width="70">
+                <h4 class="mt-3">RUTE</h4>
+            </a>
+        </div>
+
+
+        <div class="col-md-2 text-center mt-3 mb-3">
+            <a href="{{route('vehicle.index')}}" class="text-decoration-none">
+                <img src="{{asset('images/dumptruckempty.svg')}}" alt="" width="70">
+                <h4 class="mt-3">VEHICLE</h4>
+            </a>
+        </div>
+        <div class="col-md-2 text-center mt-3 mb-3">
+            <a href="{{route('database.upah-gendong')}}" class="text-decoration-none">
+                <img src="{{asset('images/upah-gendong.svg')}}" alt="" width="70">
+                <h4 class="mt-3">UPAH GENDONG</h4>
+            </a>
+        </div>
+        @endif
+        <div class="col-md-2 text-center mt-3 mb-3">
+            <a href="{{route('home')}}" class="text-decoration-none">
+                <img src="{{asset('images/dashboard.svg')}}" alt="" width="70">
+                <h4 class="mt-3">DASHBOARD</h4>
+            </a>
+        </div>
+    </div>
+
 </div>
 @endsection
