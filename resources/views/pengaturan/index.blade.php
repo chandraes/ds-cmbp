aa8516b4ddb59ac06fa69b119cdd10fd4fbc41ff
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-09-09 22:55:48 +0700

    Refactor SVG icons and home view for new features

--- resources/views/pengaturan/index.blade.php
+++ resources/views/pengaturan/index.blade.php
@@ -6,7 +6,7 @@ $password = \App\Models\PasswordKonfirmasi::first();
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12 text-center">
-            <h1><u>DASHBOARD</u></h1>
+            <h1><u>PENGATURAN</u></h1>
         </div>
     </div>
     @if (session('success'))
@@ -18,25 +18,25 @@ $password = \App\Models\PasswordKonfirmasi::first();
             )
     </script>
     @endif
-    <div class="row justify-content-left mt-5">
-        @if (auth()->user()->role === 'admin')
-        <div class="col-md-3 text-center mb-5">
+    <div class="row justify-content-left mt-5 mb-5">
+        @if (auth()->user()->role === 'admin' || auth()->user()->role === 'su')
+        <h2>PENGGUNA</h2>
+        <div class="col-md-3 text-center mt-5">
             <a href="{{route('pengguna.index')}}" class="text-decoration-none">
-                <img src="{{asset('images/worker.svg')}}" alt="" width="100">
-                <h2>Pengguna</h2>
+                <img src="{{asset('images/pengguna.svg')}}" alt="" width="70">
+                <h5 class="mt-3">AKUN</h5>
             </a>
         </div>
-        <div class="col-md-3 text-center mb-5">
+        <div class="col-md-3 text-center mt-5">
             <a href="{{route('pengaturan.wa')}}" class="text-decoration-none">
-                <img src="{{asset('images/wa.svg')}}" alt="" width="100">
-                <h2>Whatsapp</h2>
+                <img src="{{asset('images/wa.svg')}}" alt="" width="70">
+                <h5 class="mt-3">GROUP WHATSAPP</h5>
             </a>
         </div>
-        @if (in_array(auth()->user()->id, [1,2]))
-        <div class="col-md-3 text-center mb-5">
+        <div class="col-md-3 text-center mt-5">
             <a href="#" class="text-decoration-none" data-bs-toggle="modal" data-bs-target="#passwordKonfirmasi">
-                <img src="{{asset('images/password.svg')}}" alt="" width="100">
-                <h2>Password Konfirmasi</h2>
+                <img src="{{asset('images/password.svg')}}" alt="" width="70">
+                <h5 class="mt-3">PASSWORD KONFIRMASI</h5>
             </a>
             <div class="modal fade" id="passwordKonfirmasi" tabindex="-1" data-bs-backdrop="static"
                 data-bs-keyboard="false" role="dialog" aria-labelledby="pkTitle" aria-hidden="true">
@@ -69,22 +69,39 @@ $password = \App\Models\PasswordKonfirmasi::first();
                 </div>
             </div>
         </div>
+    </div>
+    <br>
+    <hr>
+    <div class="row justify-content-left">
+        <h2>OTHERS</h2>
+        <div class="col-md-3 text-center mt-5">
+            <a href="#" class="text-decoration-none">
+                <img src="{{asset('images/kosong.svg')}}" alt="" width="70">
+                <h5 class="mt-3">APLIKASI</h5>
+            </a>
+        </div>
+        <div class="col-md-3 text-center mt-5">
+            <a href="#" class="text-decoration-none">
+                <img src="{{asset('images/kosong.svg')}}" alt="" width="70">
+                <h5 class="mt-3">BATASAN</h5>
+            </a>
+        </div>
+        <div class="col-md-3 text-center mt-5">
+            <a href="#" class="text-decoration-none">
+                <img src="{{asset('images/kosong.svg')}}" alt="" width="70">
+                <h5 class="mt-3">HISTORI PESAN WA</h5>
+            </a>
+        </div>
         @endif
+        <div class="col-md-3 text-center mt-5">
+            <a href="{{route('home')}}" class="text-decoration-none">
+                <img src="{{asset('images/dashboard.svg')}}" alt="" width="70">
+                <h5 class="mt-3">DASHBOARD</h5>
+            </a>
+        </div>
+    </div>
 
-    </div>
-    {{-- <div class="col-md-3 text-center mb-5">
-        <a href="{{route('pengaturan.nota-transaksi')}}" class="text-decoration-none">
-            <img src="{{asset('images/konfigurasi.svg')}}" alt="" width="100">
-            <h2>Konfigurasi Nota Transaksi</h2>
-        </a>
-    </div> --}}
-    @endif
-    <div class="col-md-3 text-center">
-        <a href="{{route('home')}}" class="text-decoration-none">
-            <img src="{{asset('images/dashboard.svg')}}" alt="" width="100">
-            <h2>Dashboard</h2>
-        </a>
-    </div>
+
 </div>
 </div>
 @endsection
