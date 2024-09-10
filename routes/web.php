c54a4467a3f76603f83ea27664af09fcbb1ac384
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-09-10 12:23:03 +0700

    Refactor gitignore and add Legalitas models and migrations

--- routes/web.php
+++ routes/web.php
@@ -169,6 +169,23 @@ Route::group(['middleware' => ['auth']], function() {
 
 
         Route::get('statistik/perform-vendor/print', [App\Http\Controllers\StatistikController::class, 'perform_vendor_print'])->name('statistik.perform-vendor.print');
+
+        Route::prefix('legalitas')->group(function(){
+
+            Route::prefix('kategori')->group(function(){
+                Route::post('/store', [App\Http\Controllers\LegalitasController::class, 'kategori_store'])->name('legalitas.kategori-store');
+                Route::patch('/update/{id}', [App\Http\Controllers\LegalitasController::class, 'kategori_update'])->name('legalitas.kategori-update');
+                Route::delete('/destroy/{id}', [App\Http\Controllers\LegalitasController::class, 'kategori_destroy'])->name('legalitas.kategori-destroy');
+            });
+
+            Route::get('/', [App\Http\Controllers\LegalitasController::class, 'index'])->name('legalitas');
+            Route::post('/store', [App\Http\Controllers\LegalitasController::class, 'store'])->name('legalitas.store');
+            Route::patch('/update/{legalitas}', [App\Http\Controllers\LegalitasController::class, 'update'])->name('legalitas.update');
+            Route::delete('/destroy/{legalitas}', [App\Http\Controllers\LegalitasController::class, 'destroy'])->name('legalitas.destroy');
+
+            Route::post('/kirim-wa/{legalitas}', [App\Http\Controllers\LegalitasController::class, 'kirim_wa'])->name('legalitas.kirim-wa');
+
+        });
     });
 
     Route::get('billing', [App\Http\Controllers\BillingController::class, 'index'])->name('billing.index')->middleware('role:admin,user');
